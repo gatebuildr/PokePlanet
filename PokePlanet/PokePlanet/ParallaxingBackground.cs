a08327769dd44adf525941719d37845a16404735
Matt Spain <gatebuildr@gmail.com>
2014-03-05 07:29:32 -0500

    Moved everything into XNA project

---
+++ PokePlanet/PokePlanet/ParallaxingBackground.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace PokePlanet
+{
+    class ParallaxingBackground
+    {
+        Texture2D texture;
+        Vector2[] positions;
+        int speed;
+        int bgHeight;
+        int bgWidth;
+
+        public void Initialize(ContentManager content, String texturePath, int screenWidth, int screenHeight, int speed)
+        {
+            bgHeight = screenHeight;
+            bgWidth = screenWidth;
+
+            texture = content.Load<Texture2D>(texturePath);
+            this.speed = speed;
+            positions = new Vector2[screenWidth / texture.Width + 1];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = new Vector2(i * texture.Width, 0);
+            }
+        }
+        public void Update(GameTime gameTime)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i].X += speed;
+                //move background based on forward or backward speed
+                if (speed <= 0)
+                {
+                    if (positions[i].X <= -texture.Width)
+                    {
+                        positions[i].X = texture.Width * (positions.Length - 1);
+                    }
+                }
+                else
+                {
+                    if (positions[i].X >= texture.Width * (positions.Length - 1))
+                    {
+                        positions[i].X = -texture.Width;
+                    }
+                }
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                Rectangle rectBg = new Rectangle((int)positions[i].X, (int)positions[i].Y, bgWidth, bgHeight);
+                spriteBatch.Draw(texture, rectBg, Color.White);
+            }
+        }
+
+    }
+}
---
+++ PokePlanet/PokePlanet/SpriteSheet.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace PokePlanet
+{
+    [XmlRoot("img", IsNullable=false)]
+    public class SpriteSheet
+    {
+        public static SpriteSheet FromFile(ContentManager Content, String file)
+        {
+            using (var stream = File.OpenRead(file))
+            {
+                SpriteSheet sheet = FromStream(stream);
+                foreach (Definition definition in sheet.DefinitionList)
+                {
+                    foreach (SpriteDir dir in definition.DirList)
+                        foreach (SpriteCoords sprite in dir.Sprites)
+                            Console.WriteLine(sprite.Name);
+                }
+                return sheet;
+            }
+        }
+
+        public static SpriteSheet FromStream(Stream stream)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(SpriteSheet));
+            return (SpriteSheet)serializer.Deserialize(stream);
+        }
+
+        public AnimatedSprite GetAnimation(String name)
+        {
+            Console.WriteLine(SpriteList.Count + " elements in spritelist");
+            foreach(AnimatedSprite sprite in SpriteList)
+            {
+                Console.WriteLine(sprite.Name);
+                if (sprite.Name.Equals(name))
+                    return sprite;
+            }
+            Console.WriteLine("couldn't find your animation, bro");
+            return null;
+        }
+
+        [XmlAttribute("path")]
+        public String SheetPath;
+
+        [XmlElement("animatedsprite")]
+        public List<AnimatedSprite> SpriteList;
+
+        [XmlAttribute("name")]
+        public String ImageName;
+
+        [XmlElement("definitions")]
+        public List<Definition> DefinitionList;
+    }
+
+    public class Definition
+    {
+        [XmlElement("dir")]
+        public List<SpriteDir> DirList;
+    }
+
+    public class SpriteDir
+    {
+        [XmlAttribute("name")]
+        public String Name;
+
+        [XmlElement("spr")]
+        public List<SpriteCoords> Sprites;
+    }
+
+    public class SpriteCoords
+    {
+        [XmlAttribute("name")]
+        public String Name;
+
+        [XmlAttribute("x")]
+        public int x;
+
+        [XmlAttribute("y")]
+        public int y;
+
+        [XmlAttribute("w")]
+        public int Width;
+
+        [XmlAttribute("h")]
+        public int Height;
+    }
+}
